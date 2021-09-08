28aad576846624b85f7c3a9b8a4442866a1a4ed2
okko <al.tsayun@gmail.com>
2021-09-08 23:23:28 +0300

    initial commit

---
+++ common.py
@@ -0,0 +1,28 @@
+import math
+
+def getHarmonicFunction(a, maxN, phi, f):
+    return lambda n, a = a, maxN = maxN, phi = phi, f = f: a * math.sin(2 * math.pi * f * n / maxN + phi)
+
+def getPowersOfTwo(stop, start):
+    return list(map(lambda x: 2 ** x, range(stop, start)))
+
+def getpip():
+    pass
+
+def parsePiExpression(str):
+    try:
+        return eval(str, {"pi": math.pi})
+    except:
+        raise ValueError
+        
+def parseArray(str):
+    return list(map(parsePiExpression, str.replace(" ", "").split(",")))
+
+def parseNatural(str):
+    try:
+        value = int(str)
+    except:
+        raise ValueError
+    if (value < 1):
+        raise ValueError
+    return value
